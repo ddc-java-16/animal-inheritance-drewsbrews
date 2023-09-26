05fb922b666d756ad2fe740db973c31bb2dce8f2
drewsbrews <143551649+drewsbrews@users.noreply.github.com>
2023-09-26 10:27:48 -0600

    initial commit with a few classes

---
+++ src/main/java/edu/cnm/deepdive/inheritance/model/Canis.java
@@ -0,0 +1,22 @@
+package edu.cnm.deepdive.inheritance.model;
+
+public abstract class Canis {
+
+  private int weight;
+
+  public abstract void hunt ();
+
+  public abstract void hunt (String prey);
+
+  public static void describe() {
+    System.out.println("i am an instance of the Canis class.");
+  }
+
+  public int getWeight() {
+    return weight;
+  }
+
+  public void setWeight(int weight) {
+    this.weight = weight;
+  }
+}
---
+++ src/main/java/edu/cnm/deepdive/inheritance/model/Dog.java
@@ -0,0 +1,20 @@
+package edu.cnm.deepdive.inheritance.model;
+
+public class Dog extends Wolf {
+
+  @Override
+  public void hunt() {
+    super.hunt();
+    System.out.println("Hunt according to how i was trained in obedience school.");
+  }
+
+  @Override
+  public void hunt(String prey) {
+    super.hunt(prey);
+    System.out.println("chase, catch and play with" + prey);
+  }
+
+  public static void describe() {
+    System.out.println("I am an instance of the dog class.");
+  }
+}
